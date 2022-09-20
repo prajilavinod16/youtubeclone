a25d391d1a4e74c81faf97a9efa26bf7cfa61774
kavyak <kavyasreeak7@gmail.com>
2022-09-20 17:27:20 +0530

    ytube

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import{ Navbar,Feed,VideoDetail,ChannelDetail,SearchFeed  } from './components'
+
+
+
+const app = () => (
+    <BrowserRouter>
+    <Box sx={{backgroundcolor:'#000' }}>
+        <Navbar/>
+        <Routes>
+            <Route path="/" exact element={<Feed/>}/>
+            <Route path="/video/:id" element={<VideoDetail/>}/>
+            <Route path="/channel/:id" element={<ChannelDetail/>}/>
+            <Route path="/search/:searchTerm" element={<SearchFeed/>}/>
+        </Routes>
+    </Box>
+    
+    </BrowserRouter>
+  );
+
+
+export default app
